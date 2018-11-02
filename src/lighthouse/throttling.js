bf336574dbd55d9f16f54e5739c3051ceb2b47c3
Aaron <asilvas@godaddy.com>
2018-11-02 11:05:39 -0700

    backward compatibility for legacy 3G preset

--- src/lighthouse/throttling.js
+++ src/lighthouse/throttling.js
@@ -6,3 +6,8 @@ const { DEVTOOLS_RTT_ADJUSTMENT_FACTOR, DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR } 
 module.exports = Object.assign({
 
 }, throttling);
+
+if (!module.exports.mobile3G && module.exports.mobileSlow4G) {
+  // backward compatibility since LH renamed 3G
+  module.exports.mobile3G = module.exports.mobileSlow4G;
+}
